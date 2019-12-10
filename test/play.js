6bd8bac770b37846d011fe1d025e02d2db20e7bf
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2019-12-10 21:35:14 +0100

    split play/queue tests

--- test/play.js
+++ test/play.js
@@ -54,7 +54,7 @@ describe('Play', () => {
                 return new Promise((resolve, reject) => {
                     const events = [];
 
-                    kf.play('ball-roll 0.1s linear 0s 5 normal forwards', {
+                    kf.play('ball-roll 0.05s linear 0s 5 normal forwards', {
                         onBeforeStart: () => {
                             events.push(1);
                         },
@@ -90,63 +90,6 @@ describe('Play', () => {
         });
     });
 
-    describe('#queue()', () => {
-        describe('Queueing', () => {
-            jsdom({ url: "http://localhost" });
-            it('Should be able to queue animations', () => {
-                const kf = new Keyframes(document.createElement("div"));
-                kf.queue(['ball-roll 0.1s']);
-                assert(animationIncludesTest(kf.queueStore[0], ['ball-roll', '0.1s']));
-            });
-
-            it('Should add multiple animations to the queue', () => {
-                const kf = new Keyframes(document.createElement("div"));
-                kf.queue(['ball-roll 0.1s']);
-                kf.queue('ball-roll2 0.1s');
-                assert.equal(kf.queueStore.length, 2);
-            });
-        });
-       
-        describe('Playing', () => {
-            before(preload);
-            it('Should play last animation onEnd', async () => {
-                const animation = await browser.evaluate(async () => {
-                    return new Promise((resolve, reject) => {
-                        kf.resetQueue().then(() => {
-                            kf.queue(['ball-roll 0.1s'], {
-                                onEnd: () => {
-                                    resolve(window.elem.style.animation);
-                                }
-                            });
-                            kf.queue('ball-roll2 0.1s');
-                        });
-                    });
-                });
-                assert(animationIncludesTest(animation, ['ball-roll2', '0.1s']));
-            });
-        });
-    });
-
-    describe('#loop()', () => {
-        before(preload);
-        it('Should be able to loop animations', async () => {
-            const animation = await browser.evaluate(async () => {
-                return new Promise((resolve, reject) => {
-                    let count = 0;
-                    kf.loop(['ball-roll 0.1s', 'ball-roll2 0.1s'], {
-                        onEnd: () => {
-                            if (count > 1) {
-                                resolve(true);
-                            } else {
-                                count += 1;
-                            }
-                        }
-                    });
-                });
-            });
-            assert(animation);
-        });
-    });
 });
 
 module.exports = {
